7c1d8d7fad203ee4ceec0dc4332255e7188fb289
Elias Mäkinen <56360185+Eltsu22222@users.noreply.github.com>
2025-03-19 14:36:43 +0200

    Ok

---
+++ Assets/Scripts/ResetWhenTooLow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+public class ResetWhenTooLow : MonoBehaviour
+{
+    public float lowestAllowed = -10.0f;
+    void Update()
+    {
+        if (transform.position.y < lowestAllowed)
+        {
+            transform.position = Vector3.zero;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
